7866134603e9c5932970470bd10e756417a884e1
Wahab440 <146324863+Wahab440@users.noreply.github.com>
2024-12-14 12:35:05 +0500

    Delete LibraryManagementApp directory

--- LibraryManagementApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.librarymanagementapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    exports com.example.librarymanagementapp;  // Export the package for access by JavaFX FXML loader
-    opens com.example.librarymanagementapp to javafx.fxml;  // Allow javafx.fxml to access the package's contents
-}
