f5f334c406dc8ad2a52a7c9cdb0a4152d59d3547
Wahab440 <146324863+Wahab440@users.noreply.github.com>
2024-12-29 23:12:46 -0800

    Delete LibraryManagementApp directory

--- LibraryManagementApp/src/main/java/com/example/librarymanagementapp/Models/Librarian.java
+++
@@ -1,7 +0,0 @@
-package com.example.librarymanagementapp.Models;
-
-public class Librarian extends User{
-    public Librarian(String Username, String Password) {
-        super(Username, Password);
-    }
-}
--- LibraryManagementApp/src/main/java/com/example/librarymanagementapp/Models/Member.java
+++
@@ -1,7 +0,0 @@
-package com.example.librarymanagementapp.Models;
-
-public class Member extends User{
-    public Member(String Username, String Password) {
-        super(Username, Password);
-    }
-}
